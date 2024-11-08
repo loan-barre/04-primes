fc60bfee5f38c1a203fd0191e6b456cd5cc79917
loan-barre <loan.barre@edu.esiee.fr>
2024-11-08 14:17:54 +0000

    Exercice 04-primes

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
+"""
+Programme permettant de savoir si un nombre entier est premier
+"""
 from math import sqrt
+
 
 #### Fonction secondaire
 
-
 def isprime(p):
+    """
+    Determine si le nombre est premier
 
-    # votre code ici
+    Arg:
+    p: valeur entière
+    
+    Returns: True/False
+    """
+    index = int(sqrt(p))
+    if p==1:
+        return False
+    if p==2:
+        return True
+    for d in range(2, index+1):
+        if p%d == 0:
+            return False
+    return True
 
-    pass
+
 
 #### Fonction principale
 
-
 def main():
-
+    """
+    Permet l'appel des fonctions du programme
+    """
     # vos appels à la fonction secondaire ici
+    isprime(3)
+    isprime(37)
+    isprime(1000345)
+    isprime(19)
 
     for n in range(100):
         if isprime(n):
